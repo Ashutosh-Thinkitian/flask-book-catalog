7558ccf0d508cb94501f255f1be0691e8ad93e77
Ashutosh-Thinkitian <ashutosh.patil@thinkitive.com>
2021-08-10 20:02:11 +0530

    made correction

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/flask'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS=False
--- run.py
+++ run.py
@@ -2,15 +2,13 @@ from app import create_app, db
 
 from app.auth.models import User
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
+flask_app = create_app('prod')
 
-    # flask gives us to ability to run multiple application with each set of config
-    # tell to flask use current application context
-    with flask_app.app_context():
-        db.create_all()
+# flask gives us to ability to run multiple application with each set of config
+# tell to flask use current application context
+with flask_app.app_context():
+    db.create_all()
 
-        if not User.query.filter_by(user_name='ashu').first():
-            User.create_user(user='ashu', email='ashutosh.patil@thinkitive.com', password='11121997')
-
-    flask_app.run()
+    if not User.query.filter_by(user_name='ashu').first():
+        User.create_user(user='ashu', email='ashutosh.patil@thinkitive.com', password='11121997')
+        flask_app.run()
